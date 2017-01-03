705f5f838d66d92ac84451276724424dc53df175
Trevor Blades <tdblades@gmail.com>
2017-01-02 23:50:43 -0800

    Get a basic save operation running

---
+++ app.js
@@ -0,0 +1,48 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const fs = require('fs');
+const jsonfile = require('jsonfile');
+const uniqid = require('uniqid');
+
+const PROJECT_DIR = __dirname + '/projects';
+
+const app = express();
+app.enable('trust proxy');
+app.use(bodyParser.json({
+  limit: '10mb'
+}));
+app.use(cors({
+  origin: /^https?:\/\/(localhost(:\d{4})?)$/,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
+
+app.route('/projects')
+  .get(function(req, res) {
+    fs.access(PROJECT_DIR, function(err) {
+      if (err) {
+        fs.mkdirSync(PROJECT_DIR);
+      }
+
+      fs.readdir(PROJECT_DIR, function(err, files) {
+        res.send(files);
+      });
+    });
+  })
+  .post(function(req, res) {
+    fs.access(PROJECT_DIR, function(err) {
+      if (err) {
+        fs.mkdirSync(PROJECT_DIR);
+      }
+
+      const id = uniqid.process();
+      jsonfile.writeFile(`${PROJECT_DIR}/${id}.json`, req.body, function(err) {
+        if (err) {
+          return res.sendStatus(500);
+        }
+        res.send(id);
+      });
+    });
+  });
+
+app.listen(process.env.PORT || 8000);
