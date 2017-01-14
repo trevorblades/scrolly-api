9513308140bc9bb96f289de2bb5b5a75e9d5f467
Trevor Blades <tdblades@gmail.com>
2017-01-14 15:57:49 -0800

    Remove projects from gitignore and add create table statements in sql directory

---
+++ sql/projects.sql
@@ -0,0 +1,12 @@
+create table projects (
+  id serial primary key,
+  user_id int not null references users(id),
+  slug varchar(12) not null unique,
+  name varchar(120) not null,
+  width int not null,
+  height int not null,
+  layers text not null,
+  assets text not null,
+  created_at timestamp not null,
+  updated_at timestamp not null
+);
