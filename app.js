4920d02ceb9a98150a23cb551e747f8f5a8a25aa
Trevor Blades <tdblades@gmail.com>
2017-01-14 13:08:34 -0800

    Set up user creation endpoint and make auth work with the DB

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const pg = require('pg');
 const uniqid = require('uniqid');
 
@@ -8,6 +10,9 @@ const splitObject = require('./util/split-object');
 const uploadAssets = require('./util/upload-assets');
 
 const MAX_BODY_SIZE = '10mb';
+const SALT_ROUNDS = 10;
+const TOKEN_SECRET = 'LSD06vJP35py91chUfz24H5S9c';
+const TOKEN_EXPIRY = '7 days';
 
 pg.defaults.ssl = process.env.NODE_ENV === 'production';
 
@@ -22,11 +27,74 @@ app.use(cors({
 }));
 
 app.post('/auth', function(req, res) {
-  if (req.body.email !== 'email' || req.body.password !== 'password') {
-    return res.sendStatus(401);
+  if (!req.body.email || !req.body.password) {
+    return res.sendStatus(400);
   }
-  const token = 'dfasadfs';
-  res.send(token);
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if (err) {
+      return res.sendStatus(500);
+    }
+
+    const query = 'SELECT id, password, email, created_at, updated_at FROM users WHERE email LIKE $1';
+    client.query(query, [req.body.email], function(err, result) {
+      done();
+      if (err) {
+        return res.sendStatus(500);
+      } else if (!result.rows.length) {
+        return res.sendStatus(401);
+      }
+
+      const user = result.rows[0];
+      bcrypt.compare(req.body.password, user.password)
+        .then(function(success) {
+          if (!success) {
+            throw new Error();
+          }
+
+          delete user.password;
+          const token = jwt.sign(user, TOKEN_SECRET, {expiresIn: TOKEN_EXPIRY});
+          res.send(token);
+        })
+        .catch(function() {
+          return res.sendStatus(401);
+        });
+    });
+  });
+});
+
+app.post('/users', function(req, res) {
+  if (!req.body.email ||
+      !req.body.password ||
+      req.body.password !== req.body.passwordConfirm) {
+    return res.sendStatus(400);
+  }
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if (err) {
+      return res.sendStatus(500);
+    }
+
+    bcrypt.hash(req.body.password, SALT_ROUNDS)
+      .then(function(hash) {
+        const timestamp = new Date().toISOString();
+        const user = {
+          email: req.body.email,
+          password: hash,
+          created_at: timestamp,
+          updated_at: timestamp
+        };
+        const {keys, values} = splitObject(user, true);
+        const query = `INSERT INTO users(${keys.join(', ')})
+            VALUES (${keys.map((key, index) => `$${index + 1}`).join(', ')})`;
+        client.query(query, values, function(err, result) {
+          done();
+          if (err) {
+            return res.sendStatus(500);
+          }
+          res.sendStatus(200);
+        });
+      });
+  });
 });
 
 app.route('/projects')
@@ -35,7 +103,9 @@ app.route('/projects')
       if (err) {
         return res.sendStatus(500);
       }
-      client.query('SELECT id, slug, name, width, height, layers::json, assets::json, step, created_at, updated_at FROM projects', function(err, result) {
+
+      const query = 'SELECT id, slug, name, width, height, layers::json, assets::json, step, created_at, updated_at FROM projects';
+      client.query(query, function(err, result) {
         done();
         if (err) {
           return res.sendStatus(500);
@@ -82,7 +152,8 @@ app.get('/projects/:slug', function(req, res) {
     if (err) {
       return res.sendStatus(500);
     }
-    const query = 'SELECT id, slug, name, width, height, layers::json, assets::json, step, created_at, updated_at FROM projects WHERE slug = $1';
+
+    const query = 'SELECT name, width, height, layers::json, assets::json, step, created_at, updated_at FROM projects WHERE slug = $1';
     client.query(query, [req.params.slug], function(err, result) {
       done();
       if (err) {
