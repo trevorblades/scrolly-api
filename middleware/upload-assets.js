fe0caa2ce04c9036325c403c2c2577c272c5570d
Trevor Blades <tdblades@gmail.com>
2017-03-27 15:22:00 -0700

    Don't bother uploading files if no assets are provided

--- middleware/upload-assets.js
+++ middleware/upload-assets.js
@@ -7,33 +7,33 @@ const ASSET_REGEX = /^data:image\/([\w\+]+);base64,(.+)$/;
 module.exports = function(req, res, next) {
   req.assets = [];
 
-  let assetsRemaining = 0;
-  const s3 = new aws.S3();
-  req.body.assets.forEach(function(asset) {
-    const matches = ASSET_REGEX.exec(asset.src);
-    if (matches) {
-      assetsRemaining++;
+  if (req.body.assets.length) {
+    let assetsRemaining = 0;
+    const s3 = new aws.S3();
+    return req.body.assets.forEach(function(asset) {
+      const matches = ASSET_REGEX.exec(asset.src);
+      if (matches) {
+        assetsRemaining++;
 
-      const fileName = `${uniqid()}.${mime.extension(asset.mimeType)}`;
-      s3.upload({
-        Bucket: 'assets.scrol.ly',
-        Key: fileName,
-        Body: Buffer.from(matches[2], 'base64'),
-        ContentType: asset.mimeType
-      }, function(err, data) {
-        if (err) {
-          return res.sendStatus(500);
-        }
-        asset.src = `https://assets.scrol.ly/${data.Key}`;
-        assetsRemaining--;
-        if (!assetsRemaining) {
-          next();
-        }
-      });
-    }
-  });
-
-  if (!assetsRemaining) {
-    next();
+        const fileName = `${uniqid()}.${mime.extension(asset.mimeType)}`;
+        s3.upload({
+          Bucket: 'assets.scrol.ly',
+          Key: fileName,
+          Body: Buffer.from(matches[2], 'base64'),
+          ContentType: asset.mimeType
+        }, function(err, data) {
+          if (err) {
+            return res.sendStatus(500);
+          }
+          asset.src = `https://assets.scrol.ly/${data.Key}`;
+          assetsRemaining--;
+          if (!assetsRemaining) {
+            next();
+          }
+        });
+      }
+    });
   }
+
+  return next();
 };
